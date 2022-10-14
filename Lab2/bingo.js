cbfbc62cdc7dea4df097c7a393c5d3f48bf69c01
ZienabAlr <r0784494@student.thomasmore.be>
2022-10-14 18:51:25 +0200

    feat(bingo.js): done cards are loaded

--- Lab2/bingo.js
+++ Lab2/bingo.js
@@ -125,6 +125,15 @@ export default class Bingo {
     // JSON.parse() will convert the string [1, 7, 8] back to an array which you can loop
     // loop over the numbers 1, 7, 8 and mark those cards as done by adding the right CSS class
     // .bingo__card--done
-    
-  }
+
+
+    if (localStorage.getItem("bingo")) {
+        let cardsWon = JSON.parse(localStorage.getItem("bingo"));
+        for (let i = 0; i < cardsWon.length; i++) {
+          let card = document.querySelector(`[data-card="${cardsWon[i]}"]`);
+          console.log(card);
+          card.classList.add("bingo__card--done");
+        }
+      }
+    }
 }
