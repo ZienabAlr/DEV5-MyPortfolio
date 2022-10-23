26a8b444bf1392a7a0159bcaf49e2504e254bb66
ZienabAlr <r0784494@student.thomasmore.be>
2022-10-23 09:37:41 +0200

    feat: function for checking our location added if the checking succeed call function gotlocation if the checking filad call function errorlocation

--- Lab3/src/ad.js
+++ Lab3/src/ad.js
@@ -1,8 +1,24 @@
 export default class Ad {
   
     constructor() {
-        console.log('Ad has been loaded');
+        //console.log('Ad has been loaded');
+
+        // check our location 
+        this.getLocation();
+       
     }
+
+    getLocation() {
+        //console.log('getLocation');
+        // get the location from the browser
+        navigator.geolocation.getCurrentPosition(this.gotlocation, this.errorlocation); // vragen aan de browser (navigator), aan het object (geolocation) om de huidige locatie (getCurrentPosition) te bepalen.
+        // als het lukt, dan de functie gotlocation uitvoeren, als het niet lukt, dan de functie errorlocation uitvoeren.
+
+       
+    }   
+    
+   
+
 }
 
 let ad = new Ad();
