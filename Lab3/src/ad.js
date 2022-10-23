774175bd1a8cddf069a40e4d7a15f6993d506052
ZienabAlr <r0784494@student.thomasmore.be>
2022-10-23 09:52:59 +0200

    feat: logitude and latitude coords are asked

--- Lab3/src/ad.js
+++ Lab3/src/ad.js
@@ -5,13 +5,18 @@ export default class Ad {
 
         // check our location 
         this.getLocation();
+        this.lat; // eigenschap latitude
+        this.long; // eigenschap longitude
        
     }
 
     getLocation() {
         //console.log('getLocation');
         // get the location from the browser
-        navigator.geolocation.getCurrentPosition(this.gotlocation, this.errorlocation); // vragen aan de browser (navigator), aan het object (geolocation) om de huidige locatie (getCurrentPosition) te bepalen.
+        navigator.geolocation.getCurrentPosition(
+            this.gotlocation.bind(this),  // bind this to the function bind will make sure that the this keyword is the same in the function (bind betekent dat je de this van de functie aanpast)
+            this.errorlocation.bind(this) 
+            ); // vragen aan de browser (navigator), aan het object (geolocation) om de huidige locatie (getCurrentPosition) te bepalen.
         // als het lukt, dan de functie gotlocation uitvoeren, als het niet lukt, dan de functie errorlocation uitvoeren.
 
        
@@ -19,12 +24,15 @@ export default class Ad {
 
     gotlocation(result) {
 
-        console.log(result);
+        this.lat = result.coords.latitude; // ik wil de latitude van de resultaten van de browser (result) en dan de coordinaten (coords) en dan de latitude (latitude)
+        this.long = result.coords.longitude; // ik wil de longitude van de resultaten van de browser (result) en dan de coordinaten (coords) en dan de longitude (longitude)
+
+        //console.log(result);
     }
 
     errorlocation(err) {
 
-        console.log(err);
+        //console.log(err);
 
     }
     
