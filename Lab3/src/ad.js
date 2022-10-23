1e0ef86dedd4ece3518e0401497409076f815150
ZienabAlr <r0784494@student.thomasmore.be>
2022-10-23 11:00:11 +0200

    feat: api key added to the constructor, getWeather added trying to get the weather when we get the location

--- Lab3/src/ad.js
+++ Lab3/src/ad.js
@@ -1,8 +1,10 @@
 export default class Ad {
   
-    constructor() {
+    constructor(api_key) {
         //console.log('Ad has been loaded');
 
+        this.apiKey = api_key; // de api key die we hebben gekregen van de api hierboven in de constructor zetten
+        console.log(this.api_key);
         // check our location 
         this.getLocation();
         this.lat; // eigenschap latitude
@@ -12,6 +14,7 @@ export default class Ad {
 
     getLocation() {
         //console.log('getLocation');
+
         // get the location from the browser
         navigator.geolocation.getCurrentPosition(
             this.gotlocation.bind(this),  // bind this to the function bind will make sure that the this keyword is the same in the function (bind betekent dat je de this van de functie aanpast)
@@ -28,8 +31,43 @@ export default class Ad {
         this.long = result.coords.longitude; // ik wil de longitude van de resultaten van de browser (result) en dan de coordinaten (coords) en dan de longitude (longitude)
 
         //console.log(result);
-        console.log(this.lat);
-        console.log(this.long);
+        //console.log(this.lat);
+        //console.log(this.long);
+
+        // call the function to get the weather als we de locatie hebben
+        this.getWeather();
+    }
+
+    getWeather() {
+        //https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely
+        let url =`https://api.weatherbit.io/v2.0/${this.lat}&${this.long}&key=${this.apiKey}&include=minutely`;
+        
+        fetch(url)
+        .then((response)=>response.json())
+        .then((data)=> {
+            
+            //then is een promise, een belofte dat de data er is dat die ons een response geeft
+
+            //localStorage.setItem("weather", JSON.stringify(data));
+
+            console.log (response); 
+            //localStorage.setItem("timestamp", Date.now());
+            console.log(data);
+            //this.displayWeather(data);
+        });
+
+
+       
+
+              
+      
+
+          
+
+        
+        // api key 8119c4ee4bd64d62bcb4e4b7388d7932
+
+
     }
 
     errorlocation(err) {
