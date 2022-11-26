ac1ed00900ad4ae6c8dbf4c4c7d781728721ca32
ZienabAlr <r0784494@student.thomasmore.be>
2022-11-26 19:55:11 +0100

    feat: reactive array messages added

--- VUE3-TIKTOK-A/src/components/comments.vue
+++ VUE3-TIKTOK-A/src/components/comments.vue
@@ -1,11 +1,39 @@
 <script setup>
+import {onMounted, reactive, ref} from 'vue';
+
+let messages = reactive([]); // hier maak ik een array waar ik de info van de messages in kan stoppen (namen en comments)"reactive" is een functie die een object maakt dat reageert op veranderingen in de data die erin zit 
+
+onMounted(() => { // hier maak ik een functie die de data van de api ophaalt en in de array stopt
+    
+  const api_url =' https://lab5-p379.onrender.com/api/v1/messages/';
+  fetch(api_url)
+  .then((response) => response.json()) // parse JSON from request into native JavaScript objects json is a method of the response object that returns a promise that resolves with the result of parsing the body text as JSON 
+  .then((data) => {
+
+    data.forEach((item) => { // de data bestaat uit een array met daarin objecten, voor elk object in de array voer ik de volgende code uit
+      
+
+      messages.push(item);// ik push de objecten in de array messages
+
+    }); 
+  
+  });
+  
+})
+
+
+
 
 </script>
 
 <template>
   <div>
-    <h3>username</h3>
-    <p>comment</p>
+    <!-- hier haal ik de namen en comments uit de array messages en zet ik elke message in een ul  -->
+    <!-- In elke message vraag ik de user (username) en de text (de comment) -->
+    <ul v-for="item in messages" :key="item">
+      <h4>{{item.user}}</h4>
+      <p>{{item.text}}</p>
+    </ul>
    
   </div>
  
