947f035a44064bc0554c6d1641081c8da927cce9
ZienabAlr <r0784494@student.thomasmore.be>
2022-11-27 09:43:33 +0100

    feat(comments.vue): variable text has been made

--- VUE3-TIKTOK-A/src/components/comments.vue
+++ VUE3-TIKTOK-A/src/components/comments.vue
@@ -2,22 +2,27 @@
 import {onMounted, reactive, ref} from 'vue';
 
 let messages = reactive([]); // hier maak ik een array waar ik de info van de messages in kan stoppen (namen en comments)"reactive" is een functie die een object maakt dat reageert op veranderingen in de data die erin zit 
+let text = ref(''); // hier maak ik een variabele aan waar ik de text in kan stoppen
 
-onMounted(() => { // hier maak ik een functie die de data van de api ophaalt en in de array stopt
 
+ onMounted(() => { // hier maak ik een functie die de data van de api ophaalt en in de array stopt
+ 
   const api_url =' https://lab5-p379.onrender.com/api/v1/messages/';
   fetch(api_url)
   .then((response) => response.json()) // parse JSON from request into native JavaScript objects json is a method of the response object that returns a promise that resolves with the result of parsing the body text as JSON 
   .then((data) => {
 
-      console.log(data);
+    console.log(api_url);
+    data.forEach((item) => { // de data bestaat uit een array met daarin objecten, voor elk object in de array voer ik de volgende code uit 
+    messages.push(item);// ik push de objecten in de array messages
 
-      data.forEach((item) => { // de data bestaat uit een array met daarin objecten, voor elk object in de array voer ik de volgende code uit 
-        messages.push(item);// ik push de objecten in de array messages
-    
-      });
+    });
+
   }); 
+
 });
+
+
 
 </script>
 
@@ -30,8 +35,8 @@ onMounted(() => { // hier maak ik een functie die de data van de api ophaalt en 
       <p>{{item.text}}</p>
     </ul>
 
-    <input type="text" >
-    <button>Submit</button>
+    <input type="text" placeholder="Add comment" v-model="text" >
+    <button @click="postMessage">Post</button>
    
   </div>
  
