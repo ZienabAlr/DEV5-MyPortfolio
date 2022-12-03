c00db923c50d19df7a36dd485bffd50ea2f5f329
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-03 09:19:35 +0100

    feat(main.js): new geometry (shape) is added, earth has been added

--- Lab4/main.js
+++ Lab4/main.js
@@ -10,3 +10,10 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+// add some geometry
+// create earth
+const geometry = new THREE.SphereGeometry( 1, 32, 32 ); // (vorm) radius, width segments, height segments
+const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); // (kleur) color
+const earth = new THREE.Mesh( geometry, material ); // here we create the mesh (object) with the geometry and material 
+scene.add( earth );
