b7aae03f515adffc2a542042aadb3426411c15d4
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-07 10:40:34 +0100

    fix(main.js): creating the scene and the camera is fixed+ texture added to sphere geometry

--- Lab4/main.js
+++ Lab4/main.js
@@ -1,19 +1,24 @@
 import './style.css'
 import * as THREE from 'three';// importing three.js library
 
-
-// creating a scene
-
+//creating a scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+//creating a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// add some geometry
-// create earth
-const geometry = new THREE.SphereGeometry( 1, 32, 32 ); // (vorm) radius, width segments, height segments
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); // (kleur) color
-const earth = new THREE.Mesh( geometry, material ); // here we create the mesh (object) with the geometry and material 
-scene.add( earth );
+
+// creating sphere geometry
+const loader = new THREE.TextureLoader();
+const texture = loader.load('textures/fantasy.webp');
+const geometry = new THREE.SphereGeometry( 80, 32, 32 ); //radius, width segments, height segments
+const material = new THREE.MeshBasicMaterial( {color: 0xffffff } );
+material.map = texture;
+material.side = THREE.BackSide;
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+
